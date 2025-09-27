3eddbd9f647b84f58fdd9b00a5cec4dbbe3ec033
Sognatore-coder <kir.besedin137@gmail.com>
2025-09-27 14:29:47 +0700

    feat: solving the first task

---
+++ src/Lab2/Button.java
@@ -0,0 +1,21 @@
+public class Button {
+
+    private int CountClick;
+
+    public Button(){
+        this.CountClick = 0;
+    }
+
+    public void click(){
+        CountClick++;
+        System.out.println("Текущее кол-во кликов: " + CountClick);
+    }
+
+    public static void main(String[] args){
+        Button myBtn = new Button();
+
+        myBtn.click();
+        myBtn.click();
+        myBtn.click();
+    }
+}
