941ba407d80aed825218317aacc63b2d695cca28
Sognatore-coder <kir.besedin137@gmail.com>
2025-10-11 18:56:57 +0700

    feat: creating a maven.yml file for compiling a project locally and remotely

--- src/Lab4/Main.java
+++ src/Lab4/Main.java
@@ -12,14 +12,33 @@ import com.google.gson.reflect.TypeToken;
 
 import java.lang.reflect.Type;
 import java.util.List;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            // Чтение JSON файла
-            String jsonContent = new String(Files.readAllBytes(Paths.get("src/Lab4/book.json")));
+            String jsonContent;
+
+            // Пробуем найти файл как ресурс (для GitHub Actions)
+            InputStream inputStream = Main.class.getResourceAsStream("/Lab4/book.json");
+            if (inputStream != null) {
+                Reader reader = new InputStreamReader(inputStream);
+                StringBuilder content = new StringBuilder();
+                char[] buffer = new char[1024];
+                int charsRead;
+                while ((charsRead = reader.read(buffer)) != -1) {
+                    content.append(buffer, 0, charsRead);
+                }
+                reader.close();
+                jsonContent = content.toString();
+            } else {
+                // Если не найден как ресурс, ищем в файловой системе (для локальной разработки)
+                jsonContent = new String(Files.readAllBytes(Paths.get("src/Lab4/book.json")));
+            }
 
             // Парсинг JSON
             Gson gson = new Gson();
