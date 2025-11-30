66a259b656c6303ad69f6dd1868bac3c1d10bd42
Sognatore-coder <kir.besedin137@gmail.com>
2025-11-30 19:20:45 +0700

    feat: creating the main class for testing the application

---
+++ src/main/java/JavaFX/calculator/CalculatorApp.java
@@ -0,0 +1,28 @@
+package JavaFX.calculator;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class CalculatorApp extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/calculator.fxml"));
+        Parent root = loader.load();
+
+        Scene scene = new Scene(root, 400, 500);
+        scene.getStylesheets().add(getClass().getResource("/calculator.css").toExternalForm());
+
+        primaryStage.setTitle("Калькулятор");
+        primaryStage.setMinWidth(400);
+        primaryStage.setMinHeight(500);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
